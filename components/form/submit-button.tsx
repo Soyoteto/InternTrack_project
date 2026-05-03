dda2039d786333a39d0ee3d569fb6ce942e4a6a5
abergeret <aubin.bergeret@etudiant.univ-rennes.fr>
2026-05-03 10:42:39 +0200

    feat: creation of the login-form component and adaptation of the submit-button

--- components/form/submit-button.tsx
+++ components/form/submit-button.tsx
@@ -4,13 +4,19 @@ import * as React from "react";
 import { useFormContext } from "react-hook-form";
 import { Button, type ButtonProps } from "../ui/button";
 
+export interface SubmitButtonProps extends ButtonProps {
+  isPending?: boolean;
+}
+
 // A submit button that automatically disables itself and shows a loading state during form submission
-export const SubmitButton = ({ children, ...props }: ButtonProps) => {
+export const SubmitButton = ({ children, isPending, ...props }: SubmitButtonProps) => {
   const { formState: { isSubmitting } } = useFormContext();
 
+  const isLoading = isPending || isSubmitting;
+
   return (
-    <Button type="submit" disabled={isSubmitting} {...props}>
-      {isSubmitting ? "Loading..." : children}
+    <Button type="submit" disabled={isLoading} {...props}>
+      {isLoading ? "Loading..." : children}
     </Button>
   );
 };
