fe600b77d410a12d1fc15f5d511ccd9dadb28c63
Mac Baptiste <macbaptiste@MacBook-Air-de-Mac.local>
2026-05-26 19:03:00 +0200

    fix build

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.local' });
 
-export default defineConfig({
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'turso',
+  driver: 'turso',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-});
+};
